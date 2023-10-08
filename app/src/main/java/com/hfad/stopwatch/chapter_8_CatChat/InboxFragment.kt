c8b1a1ee70ed41af17cfb6ee5ae02850f700c498
user <sdelanss@gmail.com>
2023-10-08 21:08:54 +0300

    git pull origin master --allow-unrelated-histories

---
+++ app/src/main/java/com/hfad/stopwatch/chapter_8_CatChat/InboxFragment.kt
@@ -0,0 +1,20 @@
+package com.hfad.stopwatch.chapter_8_CatChat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hfad.stopwatch.R
+
+class InboxFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_inbox, container, false)
+    }
+
+}
--- app/src/main/java/com/hfad/stopwatch/chapter_8_CatChat/MainActivityCatChat.kt
+++ app/src/main/java/com/hfad/stopwatch/chapter_8_CatChat/MainActivityCatChat.kt
@@ -3,17 +3,57 @@ package com.hfad.stopwatch.chapter_8_CatChat
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import com.hfad.stopwatch.R
 
-class MainActivityCatChat: AppCompatActivity() {
+class MainActivityCatChat : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cat_chat_activity_main)
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)    //добавление свое панели инструментов
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)  //макет с выдвижной панелью
+        val builder = AppBarConfiguration.Builder(navController.graph)  //панель инструментов
+        builder.setOpenableLayout(drawer)  //кнопка доступа к выдвижной панели
+        val appBarConfiguration = builder.build()
+        toolbar.setupWithNavController(navController, appBarConfiguration)  //связывание панели инструментов с навигацией
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav)  //получение ссылки на нижнюю панель
+        bottomNavView.setupWithNavController(navController)  // связь с навигацией
+
+        val navView = findViewById<NavigationView>(R.id.nav_view)  //выдвижная панель
+        NavigationUI.setupWithNavController(navView, navController)  //связь с навигацией
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 
     companion object {
-        fun newIntent(packageContext: Context) : Intent {
+        fun newIntent(packageContext: Context): Intent {
             return Intent(packageContext, MainActivityCatChat::class.java)
         }
     }
